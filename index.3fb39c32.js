b31a974aa6c5b76909ef95462d1fc61ca06d6766
DawidKrupski <108096793+DawidKrupski@users.noreply.github.com>
2024-01-30 07:21:09 +0000

    Deploying to gh-pages from @ BarylStyle/Projekt-HTML-CSS@66367897707cda248ca7628af8a99ffe1e221e84 🚀

---
+++ index.3fb39c32.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[data-thanks-open]"),closeModalBtn:document.querySelector("[data-thanks-close]"),modal:document.querySelector("[data-thanks-modal]")};function t(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",t),e.closeModalBtn.addEventListener("click",t)})();
+//# sourceMappingURL=index.3fb39c32.js.map
